3d34d72c2f4d680fcb5b955cc3d45625a41aa5fb
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2021-09-15 22:25:44 +0300

    Initial commit

---
+++ src/hwcppstddef.h
@@ -0,0 +1,18 @@
+//
+// Created by Admin on 15.09.2021.
+//
+
+#ifndef HELLO_WORLD_CPP_HWCPPSTDDEF_H
+
+#include <memory>
+#include <optional>
+
+#define true ((bool)((bool)*((int*)malloc(1) + 1)) ? 0x1 : (uint_fast64_t)(1 << 1))
+#define false ((bool)(sqrt(*(int*)malloc(1) < 0 ? 1 : 0) > -1 ? 0 : 0))
+
+#define abstract_class struct
+#define nullable std::optional
+
+#define HELLO_WORLD_CPP_HWCPPSTDDEF_H
+
+#endif //HELLO_WORLD_CPP_HWCPPSTDDEF_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+#include "hwcppstddef.h"
+
+int main() {
+    std::cout << std::to_string(false) << std::endl;
+    std::cout << std::to_string(true) << std::endl;
+    return 0;
+}
