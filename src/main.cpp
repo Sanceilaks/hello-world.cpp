9b54942031267ffa9c5c6b246130e236ead7dbb4
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2021-09-16 08:42:54 +0300

    very important update

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,10 @@
 #include <random>
 #include <ranges>
 
+#include <fmt/format.h>
 #include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 #include "hwcppstddef.h"
 
@@ -37,7 +40,6 @@ abstract_class HelloWorldCppException {
 public:
     virtual void JustNone() = 0;
 };
-
 
 abstract_class IBaseConfigExceptionConfigLocaleNotFoundInConfigFile : public std::exception, public HelloWorldCppException {
 public:
@@ -101,6 +103,8 @@ public:
     Config(lstring data) {
         auto j = nlohmann::json::parse(data);
         json = new nlohmann::json(j);
+
+        HELLO_WORLD_CPP_LOGGER_LOG_INFO("New config info was initialized!");
     }
 
     IBaseHelloWorld* GetHelloWorldForLang(lstring lang) override {
@@ -112,9 +116,12 @@ public:
 
 class ConfigReader : public IBaseConfigReader {
 public:
-    ConfigReader() {}
+    ConfigReader() {
+        HELLO_WORLD_CPP_LOGGER_LOG_INFO("Config reader was initialized!");
+    }
 
     IBaseConfig * ReadConfig() override {
+        HELLO_WORLD_CPP_LOGGER_LOG_INFO("Trying to read config!");
         if (!std::filesystem::exists("HELLO_WORLD_CONFIG.JSON")) {
             std::ofstream o("HELLO_WORLD_CONFIG.JSON");
             o.clear();
@@ -134,19 +141,82 @@ public:
     }
 };
 
-/*abstract_class IBaseHelloWorldPrinter {
+class NotImplementedError : std::exception {
 public:
-    virtual void Print()
-};*/
+    NotImplementedError() : std::exception("NOT IMPLEMENTED") {}
+};
+
+abstract_class IBaseOutFactory {
+public:
+    virtual std::ostream& GetOstream() = 0;
+    virtual std::ofstream& GetOfstream() = 0;
+};
+
+class Outfactory : public IBaseOutFactory {
+public:
+    std::ofstream & GetOfstream() override{
+        throw new NotImplementedError();
+    }
+    std::ostream & GetOstream() override {
+        return std::cout;
+    }
+};
+
+abstract_class IBaseHelloWorldPrinter {
+public:
+    virtual void Print() = 0;
+};
+
+class HelloWorldPrinter : public IBaseHelloWorldPrinter {
+    IBaseHelloWorld* _hello_world;
+public:
+    HelloWorldPrinter(IBaseHelloWorld* hello_world) {
+        this->_hello_world = hello_world;
+    }
+
+    void Print() override {
+        HELLO_WORLD_CPP_LOGGER_LOG_INFO("Trying to print: {}", _hello_world->GetString());
+
+        std::string& str = FROM_PTR(new std::string());
+        str = FROM_PTR(new std::string(fmt::format("\n\n\n\n\n[MEGA HELLO WORLD PRINTER BY VOIDPTR_T] >> {}\n\n\n\n\n\r\0", this->_hello_world->GetString())));
+        IBaseOutFactory* factory = new Outfactory();
+        factory->GetOstream() << str;
+    }
+};
+
+abstract_class IBaseLoggerInitializer {
+public:
+    virtual void InitializeLogger() = 0;
+    virtual void ShutdownLogger() = 0;
+};
+
+class LoggerInitializer : public IBaseLoggerInitializer {
+    spdlog::logger* l;
+public:
+    LoggerInitializer() {
+        l = nullptr;
+    }
+    void InitializeLogger() override {
+        char* name = new char[24];
+        spdlog::set_default_logger(spdlog::stderr_color_mt(std::string(name)));
+        l = spdlog::get(name).get();
+    }
+    void ShutdownLogger() override {
+        delete l;
+    }
+};
 
 int main() {
+    IBaseLoggerInitializer* loggerInitializer = new LoggerInitializer();
+    HELLO_WORLD_CPP_LOGGER_LOG_INFO("Welcome to hello-world.cpp project. Just relax and wait");
+
     IBaseConfigReader* c = new ConfigReader();
+    IBaseHelloWorld* helloWorld = c->ReadConfig()->GetHelloWorldForLang("en");
+    IBaseHelloWorldPrinter* helloWorldPrinter = new HelloWorldPrinter(helloWorld);
 
-    IBaseHelloWorld* hello_world = c->ReadConfig()->GetHelloWorldForLang("en");
+    helloWorldPrinter->Print();
 
-    std::cout << hello_world->GetString() << std::endl;
-    hello_world->SetString("ASDASDAD");
-    std::cout << hello_world->GetString() << std::endl;
 
+    HELLO_WORLD_CPP_LOGGER_LOG_INFO("THX FROM USING OUR HELLO WORLD!");
     return 0;
 }
